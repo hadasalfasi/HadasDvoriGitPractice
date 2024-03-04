2a7d3473e54006cc06ef70e0479de2f7e5e3a3a1
DvoraMakovetzky <dvora2035@gmail.com>
2024-03-04 16:40:33 +0200

    jjj

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,11 +4,9 @@
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            
-=======
+
             string school = "tamar";
->>>>>>> 96222a5bd91430cbd45a1403098abbd8fbf3d615
+
             Console.WriteLine("Hello, World!");
         }
     }
